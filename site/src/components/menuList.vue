323e5e0d7547163071d9f23c40f33710e8c0ebd0
MrDrummer <mrdrummer.programming@hotmail.com>
2020-12-05 20:39:26 +0000

    Add generic menu list

---
+++ site/src/components/menuList.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  q-list.main-menu(
+    bordered
+    separator
+  )
+    q-item(
+      v-for="(item, index) in items"
+      :key="index"
+      :class="$route.path.startsWith(item.link) ? 'link-match' : ''"
+      clickable
+      tag="a"
+      :href="item.link"
+      :aria-label="item.title"
+    )
+      q-item-section(
+        v-if="item.icon"
+        avatar
+      )
+        q-avatar
+          q-icon(
+            :name="item.icon"
+          )
+      q-item-section
+        q-item-label {{ item.title }}
+      //- q-item-section(
+      //-   v-if="item.external"
+      //-   avatar
+      //- )
+      //-   q-avatar
+      //-     q-icon(
+      //-       name="fas fa-external-link-alt"
+      //-       size="xs"
+      //-     )
+      q-tooltip(
+        v-if="item.caption"
+      ) {{ item.caption }}
+</template>
+
+<script>
+export default {
+  name: 'menuList',
+  props: {
+    items: {
+      type: Array
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.link-match
+  font-weight bold
+</style>
